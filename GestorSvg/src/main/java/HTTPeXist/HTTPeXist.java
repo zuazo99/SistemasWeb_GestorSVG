d64573141f3b59588f71e699e5f2a3def59d9f5e
zuazo99 <gzuazo63@gmail.com>
2022-04-15 19:53:31 +0200

    Avanzando

--- GestorSvg/src/main/java/HTTPeXist/HTTPeXist.java
+++ GestorSvg/src/main/java/HTTPeXist/HTTPeXist.java
@@ -101,7 +101,7 @@ public class HTTPeXist {
 
 		String codigoBase64 = getAuthorizationCode("admin", "admin");
 		connect.setRequestProperty("Authorization", "Basic " + codigoBase64);
-		connect.setRequestProperty("ContentType", "aplication/xml");
+		connect.setRequestProperty("ContentType", "application/xml");
 
 		StringBuilder postData = new StringBuilder();
 		String cadena = "";
@@ -126,6 +126,32 @@ public class HTTPeXist {
 
 	}
 
+	/*-->SUBIR recurso en un String */
+	public int subirString(String collection, String resource, String resourceName) throws IOException {
+		int status = 0;
+
+		System.out.println("-->SUBIR: " + resourceName + " a " + collection);
+		if (resource.isEmpty()) {
+			System.err.println("-->SUBIR: Cannot read String ");
+			return -1;
+		}
+		String document = resourceName;
+		URL url = new URL(
+				this.server + "/exist/rest" + XmldbURI.ROOT_COLLECTION_URI + "/" + collection + "/" + document);
+		System.out.println("-->SUBIR-url: " + url);
+		HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+		connect.setRequestMethod("PUT");
+		connect.setDoOutput(true);
+
+		String codigoBase64 = getAuthorizationCode("admin", "admin");
+		connect.setRequestProperty("Authorization", "Basic " + codigoBase64);
+		connect.setRequestProperty("ContentType", "application/xml");
+
+
+
+		return status;
+	}
+
 	/* -->DELETE borrar un recurso */
 	public int delete(String collection, String resourceName)throws IOException {
 		int status = 0;
@@ -147,15 +173,6 @@ public class HTTPeXist {
 		System.out.println("<--SUBIR: " + connect.getResponseMessage());
 		return connect.getResponseCode();
 
-	}
-
-	/*-->SUBIR recurso en un String */
-	public int subirString(String collection, String resource, String resourceName) throws IOException {
-		int status = 0;
-
-		// TODO
-
-		return status;
 	}
 
 	/* -->DELETE borrar coleccion */
@@ -186,10 +203,49 @@ public class HTTPeXist {
 	public int create(String collection) throws IOException {
 		int status = 0;
 
-		//TODO
+		String resource = new String();
+		URL url = new URL(
+				this.server + "/exist/rest" + XmldbURI.ROOT_COLLECTION_URI + "/" + collection);
+		System.out.println("-->READ-url:" + url.toString());
+		HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+		connect.setRequestMethod("PUT");
+		connect.setDoOutput(true);
 
+		/* Crear codigo de autorizacion y meter en la cabecera Authorization */
+		String codigoBase64 = getAuthorizationCode("admin", "admin");
+		connect.setRequestProperty("Authorization", "Basic " + codigoBase64);
+		//connect.setRequestProperty("ContentType", "application/xml");
+
+
+		status = connect.getResponseCode();
+		System.out.println("<--SUBIR: " + status);
+		System.out.println("<--SUBIR: " + connect.getResponseMessage());
 		return status;
 	}
+
+	public int create(String collection, String resourceName) throws IOException {
+		int status = 0;
+
+		String resource = new String();
+		URL url = new URL(
+				this.server + "/exist/rest" + XmldbURI.ROOT_COLLECTION_URI + "/" + collection + "/" + resourceName);
+		System.out.println("-->READ-url:" + url.toString());
+		HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+		connect.setRequestMethod("PUT");
+		connect.setDoOutput(true);
+
+		/* Crear codigo de autorizacion y meter en la cabecera Authorization */
+		String codigoBase64 = getAuthorizationCode("admin", "admin");
+		connect.setRequestProperty("Authorization", "Basic " + codigoBase64);
+		connect.setRequestProperty("ContentType", "application/xml");
+
+
+		status = connect.getResponseCode();
+		System.out.println("<--SUBIR: " + status);
+		System.out.println("<--SUBIR: " + connect.getResponseMessage());
+		return status;
+	}
+
 
 	/* FUNCIONES AUXILIARES */
 
@@ -228,15 +284,22 @@ public class HTTPeXist {
 
 	public static void main(String[] args)
 			throws IOException, ParserConfigurationException, SAXException, TransformerException {
+		int status = 0;
 		HTTPeXist prueba = new HTTPeXist("http://localhost:8080");
-		String resourceName = "Camion.svg";
-		String collection = "SVG_imagenes";
-		String imagen = prueba.read(collection, resourceName);
-		System.out.println("Execute method --> list()");
-		String lista = prueba.list(collection);
-		System.out.println("Execute method --> delete()");
-		int status = prueba.delete(collection, "elipse.svg");
-		System.out.println("Delete_Egoera--> " + status);
+//		System.out.println("Execute method --> create(String collection)");
+//		status = prueba.create("Prueba");
+//		System.out.println("Create_Egoera--> " + status);
+		status = prueba.subir("Prueba", "GestorSvg/imagenesSVG/elipse.svg");
+		System.out.println("Subir_Egoera--> " + status);
+
+//		String resourceName = "Camion.svg";
+//		String collection = "SVG_imagenes";
+//		String imagen = prueba.read(collection, resourceName);
+//		System.out.println("Execute method --> list()");
+//		String lista = prueba.list(collection);
+		//System.out.println("Execute method --> delete()");
+		//status = prueba.delete("Prueba");
+		//System.out.println("Delete_Egoera--> " + status);
 
 	}
 }
